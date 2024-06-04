84fb689f3458f0bfb1f7ff57ccf4cc9b7136102a
家仁 李 <2286512681@qq.com>
2024-06-05 01:26:54 +0800

    添加球体

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,7 +4,21 @@
 
 #include <iostream>
 
+bool hit_sphere(const point3& center, double radius, const ray& r) { //计算射线Ray与球体是否接触
+	// t^2d \cdot d - 2td \cdot (C-Q)+(C-Q)\cdot(C-Q)-r^2=0
+	// 圆心C，半径r，射线起点Q，射线方向d，t为未知数(射线与球体的交点)
+	vec3 oc = center - r.origin(); // 计算射线起点到球心的向量
+	auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r) {
+	if (hit_sphere(point3(0,0,-1), 0.5, r)) // 如果射线与球体相交
+		return color(1, 0, 0); // 返回红色
+
     vec3 unit_direction = unit_vector(r.direction());// 获取射线的方向，并将其转换为单位向量
     auto a = 0.5*(unit_direction.y() + 1.0);// 计算一个因子a，它的值在0到1之间。当射线的方向向上时（y分量为1），a的值为1；当射线的方向向下时（y分量为-1），a的值为0。
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);// 返回一个颜色，它是白色（1.0, 1.0, 1.0）和蓝色（0.5, 0.7, 1.0）的线性插值。当a为0时，返回白色；当a为1时，返回蓝色。当a在0和1之间时，返回两种颜色的混合。
