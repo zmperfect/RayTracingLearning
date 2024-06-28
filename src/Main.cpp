b05b92a24ae47caf24f30ff635c5846183b5013e
请听下一题 <2286512681@qq.com>
2024-06-28 18:19:45 +0800

    使用stb_image_write输出

--- src/Main.cpp
+++ src/Main.cpp
@@ -209,7 +209,7 @@ void quads() {  // 场景（四边形）
 }
 
 int main() {
-	switch(5) {
+	switch(3) {
 		case 1: bouncing_spheres();     break;
         case 2: checkered_spheres();    break;
 		case 3: earth();                break;
